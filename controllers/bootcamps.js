43c78d0ac2b622aaad9b8c5e2ee355afb542d289
Nikhil <patangenikhil08@gmail.com>
2021-06-29 01:11:14 +0530

    created MongoDB connection. integrated and was able to create and get object into DB

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,23 +1,66 @@
+const Bootcamp = require('../models/Bootcamp');
+
 //@desc     Get all bootcamps
 //@route    Get /api/v1/bootcamps
 //@access   Public
-exports.getBootcamps =  (req,res,next) => {
-    res.status(200).json({success : true , msg : `Show All bootcamps`});
+exports.getBootcamps = async (req,res,next) => {
+    try {
+        const bootcamp = await Bootcamp.find();
+        res.status(200).json({
+            success : true,
+            data : bootcamp
+        });
+    } catch (error) {
+        res.status(400).json({
+            success : false
+        })
+    }
 }
 
 
 //@desc     Get single bootcamp
 //@route    Get /api/v1/bootcamps/:id
 //@access   Public
-exports.getBootcamp =  (req,res,next) => {
-    res.status(200).json({success : true , msg : `Get bootcamp ${req.params.id}`});
+exports.getBootcamp =  async (req,res,next) => {
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id);
+        
+        if(!bootcamp)
+        {
+            return res.status(400).json({
+                success : false
+            })
+        }
+
+        res.status(200).json({
+            success : true,
+            data : bootcamp
+        });
+    } catch (error) {
+        res.status(400).json({
+            success : false
+        })
+    }
 }
 
 //@desc     Create bootcamp
 //@route    Post /api/v1/bootcamps/
 //@access   Private
-exports.createBootcamp =  (req,res,next) => {
-    res.status(200).json({success : true , msg : `Create New bootcamp`});
+exports.createBootcamp =  async(req,res,next) => {
+    try {
+        const bootcamp = await Bootcamp.create(req.body)
+
+        res.status(201).json({
+            success : true,
+            data : bootcamp
+        });
+    } catch (error) {
+        res.status(400).json({
+            success : false
+        })
+    }
+
+    
 }
 
 //@desc     Update bootcamp
